375ed1ff326f7f551a9a1c8f36a39f3a1ce06bbd
Ansgar Esztermann <aeszter@gwdg.de>
2015-04-24 10:52:52 +0200

    Mock-up (Bug #2283): remove limitation to my jobs
    Only jobs with a system hold (that we do not use) will be affected anyway.

--- src/jobs.adb
+++ src/jobs.adb
@@ -1,4 +1,3 @@
-with SGE.Utils;
 package body Jobs is
 
    procedure Append_List (Nodes : SGE.Parser.Node_List) is
@@ -14,12 +13,7 @@ package body Jobs is
    end Append_List;
 
    function Can_Run (J : Job; Props : SGE.Host_Properties.Set_Of_Properties) return Boolean is
-      use SGE.Utils; -- user names
    begin
-      if J.Get_Owner /= "aeszter " then
-         return False;
-         pragma Compile_Time_Warning (True, "Mock-up limited to aeszter's jobs");
-      end if;
       if J.Get_Minimum_Slots <= SGE.Host_Properties.Get_Cores (Props) then
          return True;
       end if;
